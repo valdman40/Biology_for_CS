7a7bc09bcf34a62d16f3115f82693817d801037b
valdman40 <valdman40@gmail.com>
2021-05-31 16:29:46 +0300

    some progress

--- ex2/main.py
+++ ex2/main.py
@@ -1,10 +1,42 @@
 import difflib
 from typing import List
+from matplotlib.colors import ListedColormap
 import numpy as np
+import random
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 
-# get grade based on how close was the solution to the desired one
-def get_grade(line: List[bool], line_rules: List[int], n: int = 10):
+def get_sum_different(l1: list[int], l2: list[int]):
+    return abs(sum(l1) - sum(l2))
+
+
+def get_bits_difference(line: List[bool], line_rules: List[int]):
+    segments = []
+    segment_length = 0
+    try:
+        for square in line:
+            if not square:  # if we're at 0
+                segments.append(segment_length)
+                segment_length = 0
+            else:
+                segment_length += 1
+        if segment_length > 0:
+            segments.append(segment_length)
+    except:
+        x =3
+
+    bits_on_difference = get_sum_different(line_rules, segments)
+    return bits_on_difference
+
+
+def get_grade_bits(line: List[bool], line_rules: List[int], n: int = 10):
+    bits_on_difference = get_bits_difference(line, line_rules)
+    grade_bits = (n - bits_on_difference) / n
+    return grade_bits
+
+
+def get_grade_sequence(line: List[bool], line_rules: List[int], n: int = 10):
     segments = []
     segment_length = 0
     for square in line:
@@ -16,10 +48,14 @@ def get_grade(line: List[bool], line_rules: List[int], n: int = 10):
     if segment_length > 0:
         segments.append(segment_length)
     sm = difflib.SequenceMatcher(None, segments, line_rules)
-    grade_sequence = sm.ratio() + 0.000001
-    bits_on_difference = abs(sum(line_rules) - sum(segments))
-    grade_bits = (n - bits_on_difference) / n
-    return grade_sequence + grade_bits
+    grade_sequence = sm.ratio()
+    return grade_sequence
+
+
+# get grade based on how close was the solution to the desired one
+def get_grade(line: List[bool], line_rules: List[int], n: int = 10):
+    retval = get_grade_bits(line, line_rules, n) + get_grade_sequence(line, line_rules, n)
+    return retval
 
 
 # returns txt file as array where each cell represent line
@@ -57,29 +93,97 @@ def get_rows_cols_from_txt_file(filename: str):
 
 
 def init_grid(N: int, p: float):
-    return np.random.choice(a=[False, True], size=(N, N), p=[p, 1 - p])
+    return np.random.choice(a=[False, True], size=(N, N), p=[p, 1 - p]).tolist()
 
+
+def get_grades(grid: list[list[bool]], rules: list[list[int]], N: int):
+    grade = dict()
+    for i, (line, rule) in enumerate(zip(grid, rules)):
+        grade[f"line_{i}"] = (line, get_grade(line, rule, n=N))
+    return grade
+
+
+best_probability = 0  # for myself to see if i got better
+
+
+# returns array of lines according to their probablity
+def display_each_value_by_probability(rows_grade: dict[str, (list[bool], int)]):
+    global best_probability  # for myself to see if i got better
+    arr = []
+    probabilities = dict()
+    total_sum = sum([x[1] for x in rows_grade.values()])
+    for key in rows_grade.keys():
+        original_line = rows_grade[key][0]
+        probability = (rows_grade[key][1] / total_sum) * 100
+        # for myself to see if i got better
+        if probability > best_probability:
+            best_probability = probability
+            print('best', best_probability)
+        probabilities[key] = (original_line, probability)
+    for val in probabilities.values():
+        line, probability = val
+        for i in range(int(probability)):
+            arr.insert(0, line)
+    return arr
+
+
+# generates child from parents details
+def get_new_child(parent1: list[bool], parent2: list[bool]):
+    place_to_cut = random.randrange(0, len(parent1))
+    new_child = parent1[:place_to_cut] + parent2[place_to_cut:]
+    return new_child
+
+
+# return new grid after changing it
+def get_new_grid_by_grades(rows_grade: dict[str, (list[bool], int)], N: int):
+    new_grid = []
+    arr = display_each_value_by_probability(rows_grade)
+    for i in range(N):
+        parent1 = random.choice(arr)
+        parent2 = random.choice(arr)
+        new_grid.append(get_new_child(parent1, parent2))
+    return new_grid
+
+
+def try_to_improve(grid: list[list[bool]], line_rules: list[list[int]]):
+    improved_grid = []
+    for line, rule in zip(grid, line_rules):
+        length = len(line)
+        if get_bits_difference(line, rule) > 0:
+            i = random.randrange(0, length)
+            line[i] = not line[i]
+        elif get_grade_sequence(line, rule, length) < 1:
+            i = random.randrange(0, length)
+            j = random.randrange(0, length)
+            line[i], line[j] = line[j], line[i]
+        improved_grid.append(line)
+    return improved_grid
+
+
+def update(frameNum, img, grid: list[list[bool]], N: int, rows_rules: list[list[int]], cols_rules: list[list[int]]):
+    improved_grid = grid
+    for _ in range(50):
+        improved_grid = try_to_improve(improved_grid, rows_rules)
+    rows_grade = get_grades(improved_grid, rows_rules, N)
+    new_grid = get_new_grid_by_grades(rows_grade, N)
+    # cols_grade = get_grades(zip(*grid), rows_rules, N)
+
+    img.set_data(new_grid)
+    return img,
 
 
 def main():
     rows, cols = get_rows_cols_from_txt_file("fish.txt")
     grid = init_grid(N=len(cols), p=0.5)
-    print(grid)
-
-    lines = [
-        [False, True, True, False, False, True, True, False, False, False],
-        [True, True, False, False, False, True, True, False, False, False],
-        [False, True, False, False, False, True, True, False, False, False],
-        [False, False, False, False, False, True, True, False, False, False],
-        [False, False, False, False, False, False, False, True, False, False],
-        [True, False, True, False, True, False, False, True, False, False],
-        [False, False, False, False, False, False, False, False, False, False],
-        [True, True, True, True, True, True, True, True, True, True]
-    ]
-    for i, line in enumerate(lines):
-        if i + 1 == 7:
-            x = 3
-        # print(i + 1, get_grade(line, input))
+    figure, axes = plt.subplots()
+    cmap = ListedColormap(['w', 'k'])
+    img = axes.imshow(grid, interpolation='nearest', cmap=cmap)
+    ani = animation.FuncAnimation(figure, update, fargs=(img, grid, len(grid), rows, cols),
+                                  frames=10,
+                                  interval=10,  # millisecond to interval
+                                  save_count=50,
+                                  repeat=True)
+    plt.show()
 
 
 if __name__ == '__main__':
