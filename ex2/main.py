81481fd3faa580cca2b30e779f8d51dbb14ce6bd
valdman40 <valdman40@gmail.com>
2021-06-07 18:52:21 +0300

    change names to match exercise

--- ex2/main.py
+++ ex2/main.py
@@ -8,23 +8,17 @@ import matplotlib.animation as animation
 import math
 
 
-def get_fitness_sequence(segments: List[int], line_rules: List[int], n: int = 10):
-    sm = difflib.SequenceMatcher(None, segments, line_rules)
-    grade_sequence = sm.ratio()
-    return grade_sequence
-
-
 class Grid(object):
-    def __init__(self, g: list[list[bool]], n, grade_given=0):
+    def __init__(self, g: list[list[bool]], n, fitness=0):
         self.grid = g
         self.N = n
-        self.grade = grade_given
+        self.fitness = fitness
 
     def get_grid(self):
         return self.grid
 
     def get_grade(self):
-        return self.grade
+        return self.fitness
 
     def get_fitness_line_By_line(self, lines: list[list[bool]], rules: list[list[int]]):
         fitness = 0
@@ -32,12 +26,15 @@ class Grid(object):
             fitness += self.get_line_fitness(line, rule)
         return fitness
 
-    def fitness(self, rows_rules: list[list[int]], cols_rules: list[list[int]]):
+    def set_fitness(self, rows_rules: list[list[int]], cols_rules: list[list[int]]):
         grid_rows_fitness = self.get_fitness_line_By_line(self.grid, rows_rules)
-        grid_cols_fitness = self.get_fitness_line_By_line(zip(*self.grid), cols_rules)
+        grid_by_cols = zip(*self.grid)
+        grid_cols_fitness = self.get_fitness_line_By_line(grid_by_cols, cols_rules)
         grid_fitness = grid_rows_fitness + grid_cols_fitness
-        self.grade = grid_fitness
-        return grid_fitness
+        self.fitness = grid_fitness
+
+    def get_fitness(self):
+        return self.fitness
 
     # get grade based on how close was the solution to the desired one
     def get_line_fitness(self, line: List[bool], line_rules: List[int]):
@@ -51,29 +48,16 @@ class Grid(object):
                 segment_length += 1
         if segment_length > 0:
             segments.append(segment_length)
-        retval = self.get_fitness_bits(segments, line_rules, self.N) + get_fitness_sequence(segments, line_rules,
-                                                                                                 self.N) * 100
+        retval = get_fitness_bits_difference(segments, line_rules, self.N) + get_list_similarity(segments, line_rules) * 100
         return retval
-
-    def get_bits_difference(self, segments: List[int], line_rules: List[int]):
-        bits_on_difference = self.get_sum_different(line_rules, segments)
-        return bits_on_difference
-
-    def get_fitness_bits(self, segments: List[int], line_rules: List[int], n: int = 10):
-        bits_on_difference = self.get_bits_difference(segments, line_rules)
-        grade_bits = (n - bits_on_difference) / n
-        return grade_bits
-
-    def get_sum_different(self, l1: list[int], l2: list[int]):
-        return abs(sum(l1) - sum(l2))
 
     def improve_line(self, line: list[bool], rule: list[int]):
         # switch 1 bit
-        if self.get_bits_difference(line, rule) > 0:
+        if get_lists_sum_difference(line, rule) > 0:
             i = random.randrange(0, self.N)
             line[i] = not line[i]
         # replace 2 bits
-        if get_fitness_sequence(line, rule, self.N) < 1:
+        if get_list_similarity(line, rule) < 1:
             i, j = random.randrange(0, self.N), random.randrange(0, self.N)
             line[i], line[j] = line[j], line[i]
 
@@ -85,12 +69,35 @@ class Grid(object):
         copied = []
         for g in self.grid:
             copied.append(g.copy())
-        return Grid(copied, self.N, self.grade)
+        return Grid(copied, self.N, self.fitness)
 
     def mutate(self, p):
         for line in self.grid:
             for i in range(self.N):
                 line[i] = random.choices([line[i], not line[i]], [p, 1 - p], k=1)[0]
+
+
+# returns similarity between 2 lists
+def get_list_similarity(segments: List[int], line_rules: List[int]):
+    sm = difflib.SequenceMatcher(None, segments, line_rules)
+    grade_sequence = sm.ratio()
+    return grade_sequence
+
+
+def get_sum_different(l1: list[int], l2: list[int]):
+    return abs(sum(l1) - sum(l2))
+
+
+def get_lists_sum_difference(segments: List[int], line_rules: List[int]):
+    bits_on_difference = get_sum_different(line_rules, segments)
+    return bits_on_difference
+
+
+# returns fitness by bits similarity
+def get_fitness_bits_difference(segments: List[int], line_rules: List[int], n: int = 10):
+    bits_on_difference = get_lists_sum_difference(segments, line_rules)
+    grade_bits = (n - bits_on_difference) / n
+    return grade_bits
 
 
 # returns txt file as array where each cell represent line
@@ -150,7 +157,7 @@ def display_each_value_by_probability(grids: list[Grid]):
 
 
 # generates child from parents details
-def get_new_grid_child(parent1: Grid, parent2: Grid, N: int):
+def cross_over(parent1: Grid, parent2: Grid, N: int):
     place_to_cut = random.randrange(0, N)
     parent1_grid = parent1.get_grid()
     parent2_grid = parent2.get_grid()
@@ -166,35 +173,36 @@ def prepare_next_generation(grids: list[Grid], N: int):
     for _ in range(len(grids)):
         grid_parent1: Grid = random.choice(arr)
         grid_parent2: Grid = random.choice(arr)
-        new_grids.append(get_new_grid_child(grid_parent1, grid_parent2, N))
+        new_grids.append(cross_over(grid_parent1, grid_parent2, N))
     return new_grids
 
 
 def calculate_grade_for_each_grid(grids: list[Grid], rows_rules: list[list[int]], cols_rules: list[list[int]]):
     best_grid: Grid = grids[0]  # init
     for grid in grids:
-        grid.fitness(rows_rules, cols_rules)  # so each grid will have grade calculated
+        grid.set_fitness(rows_rules, cols_rules)  # so each grid will have grade calculated
         if grid.get_grade() > best_grid.get_grade():
             best_grid = grid
     return best_grid
 
 
-global_girds = []
+current_gen_girds = []
 best_grid_all_gen = None
-number_of_grids = 100
+population_size = 100
 total_frames = 0
 
 
-def update(frameNum, img, N: int, rows_rules: list[list[int]], cols_rules: list[list[int]]):
-    global global_girds, best_grid_all_gen, total_frames
+# activates each life cycle
+def life_cycle(frameNum, img, N: int, rows_rules: list[list[int]], cols_rules: list[list[int]]):
+    global current_gen_girds, best_grid_all_gen, total_frames
     total_frames += 1
 
     # improve each grid
-    for grid in global_girds:
+    for grid in current_gen_girds:
         grid.improve(line_rules=rows_rules)
 
     # get best grid
-    best_grid_current_gen = calculate_grade_for_each_grid(global_girds, rows_rules, cols_rules)
+    best_grid_current_gen = calculate_grade_for_each_grid(current_gen_girds, rows_rules, cols_rules)
     if best_grid_current_gen.get_grade() > best_grid_all_gen.get_grade():
         best_grid_all_gen = best_grid_current_gen.copy()
         print('best %.2f' % best_grid_all_gen.get_grade(), "| frame number %4d" % total_frames)
@@ -202,22 +210,22 @@ def update(frameNum, img, N: int, rows_rules: list[list[int]], cols_rules: list[
     if total_frames % 200 == 0:
         print("--------frame number %4d--------" % total_frames)
 
-    global_girds = prepare_next_generation(global_girds, N)
+    current_gen_girds = prepare_next_generation(current_gen_girds, N)
     img.set_data(best_grid_all_gen.get_grid())
     return img,
 
 
 def main():
-    global global_girds, best_grid_all_gen
+    global current_gen_girds, best_grid_all_gen
     rows, cols = get_rows_cols_from_txt_file("5x5_1.txt")
     N = len(cols)  # since every1 is square
-    for _ in range(number_of_grids):
-        global_girds.append(Grid(init_grid(N=N, p=0.5), N))
-    best_grid_all_gen = global_girds[0]  # init best grid
+    for _ in range(population_size):
+        current_gen_girds.append(Grid(init_grid(N=N, p=0.5), N))
+    best_grid_all_gen = current_gen_girds[0]  # init best grid
     figure, axes = plt.subplots()
     cmap = ListedColormap(['w', 'k'])
-    img = axes.imshow(global_girds[0].get_grid(), interpolation='nearest', cmap=cmap)
-    ani = animation.FuncAnimation(figure, update, fargs=(img, N, rows, cols),
+    img = axes.imshow(current_gen_girds[0].get_grid(), interpolation='nearest', cmap=cmap)
+    ani = animation.FuncAnimation(figure, life_cycle, fargs=(img, N, rows, cols),
                                   frames=10,
                                   interval=10,  # millisecond to interval
                                   save_count=50,
